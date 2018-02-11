b0cc4d828a49d6af84654ae2dda2cb4cb0706656
EC2 Default User <ec2-user@ip-172-31-27-74.us-east-2.compute.internal>
2018-02-11 04:47:13 +0000

    code for all parts

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -28,15 +28,39 @@ def hello(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  (s=~/^[bcdfghjklmnprstvwxyz]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+    return s.to_i(2) % 4 == 0
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
+  #accessor methods
+  def get_isbn value
+   @isbn
+  end
+
+  def get_price value
+   @price
+  end
+
 end
